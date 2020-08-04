928cead19813d22163172277b4196ad1b31797f9
Thales Lima <lthales53@gmail.com>
2020-08-04 10:17:16 -0300

    adjust in url

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 const URL = window.location.hostname.includes('localhost')
   ? 'http://localhost:8080'
-  : 'https://musicflix-react.herokuapp.com/categorias';
+  : 'https://musicflix-react.herokuapp.com';
 
 export default {
   URL,
